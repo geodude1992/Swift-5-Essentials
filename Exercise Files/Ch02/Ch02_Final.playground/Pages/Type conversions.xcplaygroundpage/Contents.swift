5c0e223e1d672171d079a8c1642f5337e2a8e1e5
geodude1992 <oblivichiiipower@gmail.com>
2024-03-19 20:29:16 -0700

    commented on explicit type conversions

--- Exercise Files/Ch02/Ch02_Final.playground/Pages/Type conversions.xcplaygroundpage/Contents.swift
+++ Exercise Files/Ch02/Ch02_Final.playground/Pages/Type conversions.xcplaygroundpage/Contents.swift
@@ -17,7 +17,7 @@
 var currentGold_Double = 5.832
 
 // Explicit conversions
-var currentGold_Integer: Int = Int(currentGold_Double)
+var currentGold_Integer: Int = Int(currentGold_Double) // rounds down
 var currentGold_String = String(currentGold_Double)
 
 // Inferred conversion with operators
